cd7cae4ba06869ce75536feb141fef28c9c71fbe
Peter To <pt9386@gmail.com>
2012-01-28 00:46:03 -0500

    Refactored with server.js and index.js files

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var http = require("http");
+var http = require('http');
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();	
-}).listen(8888);
+function start() {
+	function onRequest(request, response) {
+		console.log('Request receieved.');
+		response.writeHead(200, {'Content-type': 'text/plain'});
+		response.write('Hello World');
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log('Server has started.');
+}
+
+exports.start = start;
