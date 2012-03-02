76290d68e9a95fea2e094ba8589682995e357f99
Peter To <pt9386@gmail.com>
2012-03-02 04:23:39 -0500

    Switched server.js and index.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-var http = require('http');
+var server = require('./index');
 
-function start() {
-	function onRequest(request, response) {
-		console.log('Request receieved.');
-		response.writeHead(200, {'Content-type': 'text/plain'});
-		response.write('Hello World');
-		response.end();
-	}
-
-	http.createServer(onRequest).listen(8888);
-	console.log('Server has started.');
-}
-
-exports.start = start;
+server.start();
